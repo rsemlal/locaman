5bb56edf8e2ef8c2ac8ef8799c39de73264c0617
rsemlal <reda.semlal@gmail.com>
2014-08-08 22:46:40 +0200

    Refactoring on ProjectSettings

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -8,10 +8,10 @@ import com.typesafe.sbteclipse.plugin.EclipsePlugin._
 object ProjectSettings {
 
   def defProject(name: String) = {
-    Project(name, file(name), settings = all)
+    Project(name, file(name), settings = ProjectSettings.defaults)
   }
 
-  protected lazy val all = Defaults.coreDefaultSettings ++ Seq(
+  private lazy val defaults = Defaults.coreDefaultSettings ++ Seq(
     organization := Constants.organisation,
     version := Constants.version,
 
