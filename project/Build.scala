82c749128564dc7fd2de287bcaab46b4f91686ba
rsemlal <reda.semlal@gmail.com>
2014-08-10 18:46:23 +0200

    Removing empty useless project and adding locaman-api-data and locaman-api-valuetype

--- project/Build.scala
+++ project/Build.scala
@@ -4,9 +4,8 @@ import com.typesafe.sbteclipse.plugin.EclipsePlugin._
 
 object LocamanMainBuild extends Build {
   lazy val root = (project in file("."))
-    .aggregate(common, data)
+    .aggregate(apiData, apiValueType)
 
-  lazy val common = ProjectSettings.defProject("common")
-  lazy val data = ProjectSettings.defProject("data")
-    .dependsOn(common)
+  lazy val apiValueType = ProjectSettings.defProject("api-valuetype")
+  lazy val apiData = ProjectSettings.defProject("api-data") dependsOn(apiValueType)
 }
