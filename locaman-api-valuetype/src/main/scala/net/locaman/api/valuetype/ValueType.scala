229541470d4232e488449613d44d81d36cbfd2bd
rsemlal <reda.semlal@gmail.com>
2014-08-11 22:56:28 +0200

    Some comments
    Issue #2: Implemeting Value Type Api

--- locaman-api-valuetype/src/main/scala/net/locaman/api/valuetype/ValueType.scala
+++ locaman-api-valuetype/src/main/scala/net/locaman/api/valuetype/ValueType.scala
@@ -1,5 +1,33 @@
 package net.locaman.api.valuetype
 
+object ValueType {
+  /**
+   * Extracts the fields {{{id}}} and {{{isList}}} from [[net.locaman.api.valuetype.ValueType]].
+   */
+  def unapply(o: ValueType) = {
+    Some((o.id, o.isList))
+  }
+
+  object Atomic {
+    def apply(id: String) = AtomicValueType(id)
+
+    def unapply(o: ValueType) = o match {
+      case e: AtomicValueType => Some(o.id)
+      case _ => None
+    }
+  }
+
+  object List {
+    def apply(atomicId: String) = ListValueType(Atomic(atomicId))
+    def apply(atomic: AtomicValueType) = ListValueType(atomic)
+
+    def unapply(o: ValueType) = o match {
+      case e: ListValueType => Some(o.atomicType)
+      case _ => None
+    }
+  }
+}
+
 sealed trait ValueType {
   def id: String
   def isList: Boolean
@@ -8,17 +36,17 @@ sealed trait ValueType {
   def atomicType: AtomicValueType
 }
 
-case class ListValueType(atomicType: AtomicValueType) extends ValueType {
-  def id = atomicType.id + ".list"
+case class AtomicValueType(id: String) extends ValueType {
+  override def isList = false
 
-  def isList = true
-
-  def listType = this
+  override def listType = new ListValueType(this)
+  override def atomicType = this
 }
 
-case class AtomicValueType(id: String) extends ValueType {
-  def isList = false
+case class ListValueType(atomicType: AtomicValueType) extends ValueType {
+  override def id = atomicType.id + ".list"
 
-  def listType = new ListValueType(this)
-  def atomicType = this
+  override def isList = true
+
+  override def listType = this
 }
