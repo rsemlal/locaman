eb7879333ea67055ce860ed3a5a226939142acff
rsemlal <reda.semlal@gmail.com>
2014-08-12 22:43:58 +0200

    Adding unapply to ObjectData
    Issue #1: Implementing Data Api

--- locaman-api-data/src/main/scala/net/locaman/api/data/core/ObjectData.scala
+++ locaman-api-data/src/main/scala/net/locaman/api/data/core/ObjectData.scala
@@ -2,6 +2,10 @@ package net.locaman.api.data.core
 
 import shapeless.HList
 
+object ObjectData {
+  def unapply[A <: ObjectRef, B <: HList](o: ObjectData[A, B]) = Some(o.ref, o.toHList)
+}
+
 trait ObjectData[TRef <: ObjectRef, TValues <: HList] {
   def ref: TRef
   def toHList: TValues
